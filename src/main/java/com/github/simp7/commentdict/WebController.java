412276784c25d3ca3ef84116bf9aae6c3253812d
simp7 <JHParkOfficial@protonmail.com>
2024-01-22 15:40:16 +0900

    Ready to implement gRPC.

--- src/main/java/com/github/simp7/commentdict/WebController.java
+++ src/main/java/com/github/simp7/commentdict/WebController.java
@@ -1,5 +1,6 @@
 package com.github.simp7.commentdict;
 
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import javax.servlet.http.HttpSession;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+
+import com.github.simp7.idl.auth.*;
 
 @Controller
 @RequestMapping("/")
